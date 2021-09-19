43495c489544ea4aea87ee4466aab940709fc62f
hasanarbabi <hasanarbabi90@gmail.com>
2021-09-19 14:57:54 +0430

    first commit

--- ProjexXV1/asset/js/app.js
+++ ProjexXV1/asset/js/app.js
@@ -1,4 +1,16 @@
 $(".btn").click(function show() {
+    $("#spinner").removeClass("d-none");
+    loading();
     $(".show").addClass("d-none");
     $(this.id).removeClass("d-none");
 });
+
+function loading() {
+    $.ajax({
+        type: "GET",
+        url: "https://api.jsonbin.io/b/608d7d2692cb9267d0c8fc72",
+        success: function (res) {
+            $("#spinner").addClass("d-none");
+        }
+    })
+}
